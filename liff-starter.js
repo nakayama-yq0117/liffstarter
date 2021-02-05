602e011c394d9550d93b61e00bee2cdbb0b7989e
nakayama-yq0117 <78580421+nakayama-yq0117@users.noreply.github.com>
2021-02-05 14:31:56 +0900

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    const useNodeJS = true;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    const useNodeJS = false;   / you are not using a node server, set this value to fa
+    const defaultLiffId = "1655642985-mxLd5PW9";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
